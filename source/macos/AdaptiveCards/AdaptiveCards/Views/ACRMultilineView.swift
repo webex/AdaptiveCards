215fbe046c488b4d92f417d5e63bf6d1bf09b52f
mukuagar <mukuagar@cisco.com>
2021-02-18 10:20:11 +0530

    Placeholder in multiline fixed

--- source/macos/AdaptiveCards/AdaptiveCards/Views/ACRMultilineView.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Views/ACRMultilineView.swift
@@ -1,6 +1,6 @@
 import AppKit
-
-class ACRMultilineView: NSView {
+let PLACEHOLDERTEXT = "Sample Placeholder"
+class ACRMultilineView: NSView, NSTextViewDelegate {
     @IBOutlet var contentView: NSView!
     @IBOutlet var scrollView: NSScrollView!
     @IBOutlet var textView: NSTextView!
@@ -32,13 +32,15 @@ class ACRMultilineView: NSView {
     override func awakeFromNib() {
         super.awakeFromNib()
         scrollView.borderType = .lineBorder
+        textView.delegate = self
     }
     
     func setPlaceholder(placeholder: String) {
-        let attributedPlaceholder = NSMutableAttributedString(string: placeholder)
-        attributedPlaceholder.addAttributes([.foregroundColor: NSColor.lightGray], range: NSRange(location: 0, length: attributedPlaceholder.length))
-        textView.textStorage?.setAttributedString(attributedPlaceholder)
-        }
+//        let attributedPlaceholder = NSMutableAttributedString(string: placeholder)
+//        attributedPlaceholder.addAttributes([.foregroundColor: NSColor.lightGray], range: NSRange(location: 0, length: attributedPlaceholder.length))
+//        textView.textStorage?.setAttributedString(attributedPlaceholder)
+        applyPlaceholderStyle(textView, PLACEHOLDERTEXT)
+    }
     
     func setValue(value: String) {
         let attributedValue = NSMutableAttributedString(string: value)
@@ -48,4 +50,85 @@ class ACRMultilineView: NSView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    // MARK: Trying Grokswift here
+    func applyPlaceholderStyle(_ aTextView: NSTextView, _ placeholderText: String) {
+        aTextView.textColor = NSColor.lightGray
+        aTextView.string = placeholderText
+    }
+    
+    func applyNonPlaceholderStyle(_ aTextView: NSTextView) {
+        aTextView.textColor = NSColor.black
+    }
+    
+    func textViewShouldBeginEditing(aTextView: NSTextView) -> Bool {
+        if aTextView == textView && aTextView.string == PLACEHOLDERTEXT {
+            moveCursorToStart(aTextView)
+        }
+        return true
+    }
+    
+    func moveCursorToStart(_ aTextView: NSTextView) {
+        DispatchQueue.main.async {
+            aTextView.setSelectedRange(NSRange(location: 0, length: 0))
+        }
+    }
+    
+    func textView(_ textView: NSTextView, shouldChangeTextIn range: NSRange, replacementString text: String?) -> Bool {
+        let newLength = self.textView.string.utf16.count + (text?.utf16.count ?? 0) - range.length
+        if newLength > 0 // have text, so don't show the placeholder
+        {
+          // check if the only text is the placeholder and remove it if needed
+          // unless they've hit the delete button with the placeholder displayed
+            if self.textView == textView && self.textView.string == PLACEHOLDERTEXT {
+                if ((text?.utf16.count) == 0)// they hit the back button
+            {
+              return false // ignore it
+            }
+                applyNonPlaceholderStyle(self.textView)
+                textView.string = text ?? ""
+          }
+          return true
+        } else { // no text, so show the placeholder
+            applyPlaceholderStyle(self.textView, PLACEHOLDERTEXT)
+            moveCursorToStart(textView)
+          return false
+        }
+      }
 }
+// class TextViewWithPlaceholder {
+//    static NSAttributedString *placeHolderString;
+//
+//    @implementation TextViewWithPlaceHolder
+//
+//    +(void)initialize
+//    {
+//    static BOOL initialized = NO;
+//    if (!initialized)
+//    {
+//    NSColor *txtColor = [NSColor grayColor];
+//    NSDictionary *txtDict = [NSDictionary dictionaryWithObjectsAndKeys:txtColor, NSForegroundColorAttributeName, nil];
+//    placeHolderString = [[NSAttributedString alloc] initWithString:@"This is my placeholder text" attributes:txtDict];
+//    }
+//    }
+//
+//    - (BOOL)becomeFirstResponder
+//    {
+//      [self setNeedsDisplay:YES];
+//      return [super becomeFirstResponder];
+//    }
+//
+//    - (void)drawRect:(NSRect)rect
+//    {
+//    [super drawRect:rect];
+//    if ([[self string] isEqualToString:@""] && self != [[self window] firstResponder])
+//    [placeHolderString drawAtPoint:NSMakePoint(0,0)];
+//    }
+//
+//    - (BOOL)resignFirstResponder
+//    {
+//      [self setNeedsDisplay:YES];
+//      return [super resignFirstResponder];
+//    }
+//
+//    @end
+// }
