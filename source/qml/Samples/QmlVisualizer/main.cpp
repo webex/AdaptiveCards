6c93d73ad49cb7e76293516731d850325a1adfaa
dgharde <dgharde@cisco.com>
2021-02-22 19:23:41 +0530

    Added Date Input Render function

--- source/qml/Samples/QmlVisualizer/main.cpp
+++ source/qml/Samples/QmlVisualizer/main.cpp
@@ -385,6 +385,38 @@ const std::string card_richText = R"({
   ]
 })";
 
+  const std::string card_dateInput = R"({
+	"$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+	"type": "AdaptiveCard",
+	"version": "1.0",
+	"body": [
+		{
+		  "type": "TextBlock",
+		  "text": "Date Format: MM/DD/YYYY"
+		},
+		{
+		  "type": "Input.Date",
+		  "id": "sample",
+		  "placeholder": "Enter a date",
+		  "value": "2017-10-12",
+		  "min":"1900-01-01",
+		  "max":"2030-01-01"
+		},
+	    {
+		  "type": "TextBlock",
+		  "text": "Date Input Test"
+		},
+		{
+		  "type": "Input.Date",
+		  "id": "sample2",
+		  "placeholder": "Enter a date",
+		  "value": "2017-10-12",
+		  "min":"1900-01-01",
+		  "max":"2030-01-01"
+		}
+	]
+})";
+
 static std::shared_ptr<AdaptiveCards::HostConfig> GetHostConfig()
 {
     std::shared_ptr<AdaptiveCards::HostConfig> hostConfig = std::make_shared<AdaptiveCards::HostConfig>();
@@ -438,7 +470,7 @@ int main(int argc, char* argv[])
     QQuickView view;
     QQmlContext* context = view.engine()->rootContext();
 
-    const std::string qmlString = GenerateQml(card_richText);
+    const std::string qmlString = GenerateQml(card_dateInput);
 
 	context->setContextProperty("_aQmlCard", QString::fromStdString(qmlString));
 
