94f19ea332301fa5c25f0ca1e5dd5a4a804ccc81
Ramkrishna Jana <ramjana@cisco.com>
2021-02-23 18:41:42 +0530

    Move card jason to SampleCardJson.h

--- source/qml/Samples/QmlVisualizer/samplecardlist.cpp
+++ source/qml/Samples/QmlVisualizer/samplecardlist.cpp
@@ -1,11 +1,15 @@
 #include "samplecardlist.h"
+#include "SampleCardJson.h"
 
 SampleCardList::SampleCardList(QObject *parent) : QObject(parent)
 {
-    mCards.append({ QStringLiteral("card1"), QStringLiteral("Card Json 1") });
-    mCards.append({ QStringLiteral("card2"), QStringLiteral("Card Json 2") });
-    mCards.append({ QStringLiteral("card3"), QStringLiteral("Card Json 3") });
-    mCards.append({ QStringLiteral("card4"), QStringLiteral("Card Json 4") });
+    mCards.append({ QStringLiteral("Empty card"), QString::fromStdString(Samples::card_Empty) });
+    mCards.append({ QStringLiteral("TextBlock"), QString::fromStdString(Samples::card_TextBlock) });
+    mCards.append({ QStringLiteral("Rich text"), QString::fromStdString(Samples::card_richText) });
+    mCards.append({ QStringLiteral("Input text"), QString::fromStdString(Samples::card_InputText) });
+    mCards.append({ QStringLiteral("Input number"), QString::fromStdString(Samples::card_InputNumber) });
+    mCards.append({ QStringLiteral("Input date"), QString::fromStdString(Samples::card_dateInput) });
+    mCards.append({ QStringLiteral("Input Toggle"), QString::fromStdString(Samples::card_CheckboxInput) });
 }
 
 QVector<Card> SampleCardList::cardList() const
