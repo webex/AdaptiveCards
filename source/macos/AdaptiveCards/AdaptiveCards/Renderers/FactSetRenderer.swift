16ff88f8c8d87de5e1f432a8726a3f69c4aa951b
mukuagar <mukuagar@cisco.com>
2021-02-23 09:29:32 +0530

    Fixed text alignment problem when title is nil

--- source/macos/AdaptiveCards/AdaptiveCards/Renderers/FactSetRenderer.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Renderers/FactSetRenderer.swift
@@ -9,18 +9,12 @@ class FactSetRenderer: NSObject, BaseCardElementRendererProtocol {
             logError("Element is not of type ACSFactSet")
             return NSView()
         }
-//        let factSetView = ACRFactSetView()
         let factArray = factSet.getFacts()
-//        let view1 = [ACRFactSetElement()]
         let columnStack = NSStackView()
         columnStack.orientation = .vertical
         columnStack.translatesAutoresizingMaskIntoConstraints = false
-//        columnStack.alignment = .centerX
-//        columnStack.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
-//        columnStack.heightAnchor.constraint(equalToConstant: 200.0).isActive = true
         
         for fact in factArray {
-//            print(fact.getTitle(), fact.getValue())
             let view = ACRFactSetElement()
             view.setLabel(string: fact.getTitle())
             view.setValue(string: fact.getValue())
@@ -30,35 +24,8 @@ class FactSetRenderer: NSObject, BaseCardElementRendererProtocol {
             }
             view.labelText.preferredMaxLayoutWidth = columnStack.frame.width / 2
             view.valueText.preferredMaxLayoutWidth = columnStack.frame.width / 2
-//            return view
             columnStack.addView(view, in: .bottom)
         }
-//        columnStack.layout()
-//        columnStack.setupConstraints()
-//        columnStack.stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         return columnStack
-    }
-}
-
-class CustomStackView: NSView {
-    public var stackView = NSStackView()
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-//        setupConstraints()
-    }
-    public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    public func setupConstraints() {
-        self.stackView.orientation = .vertical
-        self.stackView.alignment = .centerX
-        self.stackView.translatesAutoresizingMaskIntoConstraints = false
-//        print(self.stackView.intrinsicContentSize)
-//        self.stackView.setClippingResistancePriority(.fittingSizeCompression, for: .vertical)
-//        self.stackView.set
-    }
-    override var intrinsicContentSize: NSSize {
-        print(self.stackView.fittingSize.height)
-        return NSSize(width: super.intrinsicContentSize.width, height: self.stackView.fittingSize.height)
     }
 }
--- source/macos/AdaptiveCards/AdaptiveCards/Views/ACRFactSetElement.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Views/ACRFactSetElement.swift
@@ -2,11 +2,14 @@ import AdaptiveCards_bridge
 import AppKit
 
 class ACRFactSetElement: NSView {
+    // Initialise the outlets
     @IBOutlet var contentView: NSView!
     @IBOutlet var labelText: NSTextField!
     @IBOutlet var valueText: NSTextField!
     @IBOutlet var horizontalLine: NSBox!
     @IBOutlet var verticalLine: NSBox!
+    
+    let hostConfig = ACSHostConfig()
     
     init() {
         super.init(frame: .zero)
@@ -28,6 +31,7 @@ class ACRFactSetElement: NSView {
     }
     
     private func setupConstaints() {
+        // TranslateAutoresizingMaskIntoConstaints as false
         contentView.translatesAutoresizingMaskIntoConstraints = false
         labelText.translatesAutoresizingMaskIntoConstraints = false
         valueText.translatesAutoresizingMaskIntoConstraints = false
@@ -38,47 +42,56 @@ class ACRFactSetElement: NSView {
         contentView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-//        contentView.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
         
         labelText.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         labelText.topAnchor.constraint(equalTo: topAnchor).isActive = true
         labelText.bottomAnchor.constraint(equalTo: horizontalLine.topAnchor).isActive = true
         labelText.trailingAnchor.constraint(equalTo: verticalLine.leadingAnchor).isActive = true
-        labelText.maximumNumberOfLines = 0
+        // This property enables us to wrap and increase the window size to fit text
+//        labelText.maximumNumberOfLines = 0
         labelText.setContentCompressionResistancePriority(.required, for: .vertical)
+//        labelText.setContentCompressionResistancePriority(.required, for: .horizontal)
+//        labelText.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+//        labelText.cell?.wraps = true
+//        labelText.cell?.usesSingleLineMode = false
+//        labelText.cell?.isScrollable = false
+//        labelText.lineBreakMode = .byWordWrapping
 
         valueText.leadingAnchor.constraint(equalTo: verticalLine.trailingAnchor).isActive = true
         valueText.topAnchor.constraint(equalTo: topAnchor).isActive = true
         valueText.bottomAnchor.constraint(equalTo: horizontalLine.topAnchor).isActive = true
         valueText.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        valueText.maximumNumberOfLines = 0
+        // Similar to labelText for wrapping
+//        valueText.maximumNumberOfLines = 0
         valueText.setContentCompressionResistancePriority(.required, for: .vertical)
+//        valueText.cell?.wraps = true
+//        valueText.cell?.usesSingleLineMode = false
+//        valueText.cell?.isScrollable = false
+        valueText.lineBreakMode = .byWordWrapping
         
         verticalLine.topAnchor.constraint(equalTo: topAnchor).isActive = true
         verticalLine.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        verticalLine.widthAnchor.constraint(equalToConstant: 10).isActive = true
+        verticalLine.isTransparent = true
         
         horizontalLine.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         horizontalLine.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         horizontalLine.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        horizontalLine.isTransparent = true
         
-        labelText.widthAnchor.constraint(equalTo: valueText.widthAnchor).isActive = true
-        labelText.widthAnchor.constraint(equalToConstant: contentView.frame.width / 2).isActive = true
+//        labelText.widthAnchor.constraint(equalTo: valueText.widthAnchor).isActive = true
+        valueText.widthAnchor.constraint(equalToConstant: contentView.frame.width / 2).isActive = true
         
-        labelText.cell?.wraps = true
-        labelText.cell?.usesSingleLineMode = false
-        labelText.cell?.isScrollable = false
-        labelText.lineBreakMode = .byWordWrapping
-//        labelText.preferredMaxLayoutWidth =
-//        valueText.preferredMaxLayoutWidth = 100
-        
-        valueText.cell?.wraps = true
-        valueText.cell?.usesSingleLineMode = false
-        valueText.cell?.isScrollable = false
-        valueText.lineBreakMode = .byWordWrapping
+        // Customize the look to add bold for label etc
+        labelText.alignment = .natural
+        valueText.alignment = .natural
     }
     
     func setLabel( string: String?) {
-        labelText.stringValue = string ?? ""
+        let randomString = NSMutableAttributedString(string: string ?? "")
+        randomString.addAttributes([.font: NSFont.boldSystemFont(ofSize: 12 )], range: NSRange(location: 0, length: randomString.length))
+        labelText.attributedStringValue = randomString
+//        labelText.stringValue = string ?? ""
     }
     
     func setValue( string: String?) {
