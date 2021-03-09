5b859d1b326d6c8c0b18ad630c944e5546b08f52
rohshar6 <rohshar6@cisco.com>
2021-03-09 19:05:07 +0530

    added new testcase
    Signed-off-by: rohshar6 <rohshar6@cisco.com>

--- source/macos/AdaptiveCards/AdaptiveCardsTests/Renderers/ContainerRendererTests.swift
+++ source/macos/AdaptiveCards/AdaptiveCardsTests/Renderers/ContainerRendererTests.swift
@@ -26,6 +26,20 @@ class ContainerRendererTests: XCTestCase {
         XCTAssertEqual(containerView.fittingSize.height, 200)
     }
     
+    func testRendererSetsVerticalContentAlignment() {
+        container = .make(verticalContentAlignment: .top)
+        var containerView = renderContainerView()
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 0)
+        
+        container = .make(verticalContentAlignment: .bottom)
+        containerView = renderContainerView()
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 1)
+        
+        container = .make(verticalContentAlignment: .center)
+        containerView = renderContainerView()
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 2)
+    }
+    
     private func renderContainerView() -> ACRColumnView {
         let view = containerRenderer.render(element: container, with: hostConfig, style: .default, rootView: NSView(), parentView: NSView(), inputs: [])
         
