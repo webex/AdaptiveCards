cc8d2575913ffc1668f625401404d5b2e2318465
mukuagar <mukuagar@cisco.com>
2021-03-09 21:14:08 +0530

    ImageSet Renderer complete

--- source/macos/AdaptiveCards/AdaptiveCards/Views/ACRCollectionViewItem.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Views/ACRCollectionViewItem.swift
@@ -1,15 +1,11 @@
 import Cocoa
 
 class ACRCollectionViewItem: NSCollectionViewItem {
-    var myImage: NSImage?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func loadView() {
-        let imageView = NSImageView()
-        imageView.wantsLayer = true
-        imageView.layer?.backgroundColor = .clear
-        view = imageView
+        view = NSView(frame: .zero)
     }
 }
