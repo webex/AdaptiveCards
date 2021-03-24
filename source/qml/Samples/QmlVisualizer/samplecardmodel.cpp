229fbddc12422449ef0282d88bf7db8012ea2582
Ramkrishna Jana <ramjana@cisco.com>
2021-03-24 19:27:00 +0530

    review fix

--- source/qml/Samples/QmlVisualizer/samplecardmodel.cpp
+++ source/qml/Samples/QmlVisualizer/samplecardmodel.cpp
@@ -115,7 +115,7 @@ QString SampleCardModel::generateQml(const QString& cardQml)
 		if (genQml.GetElement() == "Frame" && genQml.HasProperty("readonly property bool hasBackgroundImage"))
 		{
             std::string str = genQml.GetProperty("background");
-            genQml.Property("background", std::regex_replace(str, std::regex("source.*\n"), "source:\"" + getImagePath("Frame") + "\"\n"));
+            genQml.Property("background", std::regex_replace(str, std::regex("source:.*\n"), "source:\"" + getImagePath("Frame") + "\"\n"));
 		}
 		else if (genQml.GetElement() == "Image" && genQml.HasProperty("readonly property bool isImage"))
 		{
