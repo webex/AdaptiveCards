b5c8e50d973c3a8660af622c924d3c4971b9de2c
dgharde <dgharde@cisco.com>
2021-04-05 19:07:03 +0530

    Added curl_multi_perform

--- source/qml/Samples/QmlVisualizer/ImageDownloader.cpp
+++ source/qml/Samples/QmlVisualizer/ImageDownloader.cpp
@@ -30,12 +30,50 @@ bool ImageDownloader::download_jpeg(const std::string& imgName, char* url)
         return false;
 	}
 
+	CURLM* multi_handle;
+	int still_running = 0;
+	multi_handle = curl_multi_init();
+
 	CURL* curlCtx = curl_easy_init();
 	curl_easy_setopt(curlCtx, CURLOPT_URL, url);
 	curl_easy_setopt(curlCtx, CURLOPT_WRITEDATA, fp);
 	curl_easy_setopt(curlCtx, CURLOPT_WRITEFUNCTION, callbackfunction);
 	curl_easy_setopt(curlCtx, CURLOPT_FOLLOWLOCATION, 1);
 
+	curl_multi_add_handle(multi_handle, curlCtx);
+
+	curl_multi_perform(multi_handle, &still_running);
+
+	while (still_running) {
+		CURLMcode mc; /* curl_multi_wait() return code */
+		int numfds;
+		int repeats = 0;
+
+		/* wait for activity, timeout or "nothing" */
+		mc = curl_multi_wait(multi_handle, NULL, 0, 1000, &numfds);
+
+		if (mc != CURLM_OK) {
+			fprintf(stderr, "curl_multi_wait() failed, code %d.\n", mc);
+			break;
+		}
+
+		/* 'numfds' being zero means either a timeout or no file descriptors to
+		   wait for. Try timeout on first occurrence, then assume no file
+		   descriptors and no file descriptors to wait for means wait for 100
+		   milliseconds. */
+
+		if (!numfds) {
+			repeats++; /* count number of repeated zero numfds */
+			if (repeats > 1) {
+				Sleep(100); /* sleep 100 milliseconds */
+			}
+		}
+		else
+			repeats = 0;
+
+		curl_multi_perform(multi_handle, &still_running);
+	}
+	/*
 	CURLcode rc = curl_easy_perform(curlCtx);
 	if (rc)
 	{
@@ -49,7 +87,8 @@ bool ImageDownloader::download_jpeg(const std::string& imgName, char* url)
 	{
 		printf("!!! Response code: %d\n", res_code);
         return false;
-	}
+	}*/
+	curl_multi_cleanup(multi_handle);
 
 	curl_easy_cleanup(curlCtx);
 
