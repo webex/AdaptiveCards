801e896a2a83be66b621dba92cae071cc3e182c4
mukuagar <mukuagar@cisco.com>
2021-09-27 12:41:00 +0530

    Changed button colors

--- source/macos/ADCMacOSVisualizer/ADCMacOSVisualizer/Utils.swift
+++ source/macos/ADCMacOSVisualizer/ADCMacOSVisualizer/Utils.swift
@@ -27,7 +27,7 @@ extension NSColor {
         case 8: return NSColor(red: CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0,
                                green: CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0,
                                blue: CGFloat((rgbValue & 0x000000FF)) / 255.0,
-                               alpha: CGFloat(rgbValue & 0xFF000000) / 255.0)
+                               alpha: CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0)
         
         default:
             print("Not valid HexCode: \(hexString)")
@@ -37,23 +37,52 @@ extension NSColor {
 }
 
 struct Constants {
-    static let lightThemeDefault = "#007EA8"
-    static let lightThemeDefaultPressed = "#0A5E7D"
+    // HexCodeFormat: #AARRGGBB
+    static let lightThemeDefault = "#F2000000"
+    static let lightThemeDefaultBorder = "#F2000000"
+    static let lightThemeDefaultHover = "#CC000000"
+    static let lightThemeDefaultPressed = "#B2000000"
+    static let lightThemeDefaultPressedBorderColor = "#B2000000"
+    static let lightThemeDefaultTextColor = "#F2FFFFFF"
+    static let lightThemeDefaultSelectedTextColor = "#F2FFFFFF"
     
-    static let lightThemePositive = "#1B8728"
-    static let lightThemePositivePressed = "#196323"
+    static let lightThemePositive = "#FFFFFFFF"
+    static let lightThemePositiveBorder = "#FF185E46"
+    static let lightThemePositiveHover = "FF185E46"
+    static let lightThemePositivePressed = "#FF134231"
+    static let lightThemePositivePressedBorderColor = "#FF134231"
+    static let lightThemePositiveTextColor = "#FF185E46"
+    static let lightThemePositiveSelectedTextColor = "#F2FFFFFF"
     
-    static let lightThemeDestructive = "#D93829"
-    static let lightThemeDestructivePressed = "#A12C23"
+    static let lightThemeDestructive = "#FFFFFFFF"
+    static let lightThemeDestructiveBorder = "#FFAB0A15"
+    static let lightThemeDestructiveHover = "FFAB0A15"
+    static let lightThemeDestructivePressed = "#FF780D13"
+    static let lightThemeDestructivePressedBorderColor = "#FF780D13"
+    static let lightThemeDestructiveTextColor = "#FFAB0A15"
+    static let lightThemeDestructiveSelectedTextColor = "#F2FFFFFF"
     
-    static let lightThemeInline = "#E6E8E8"
-    static let lightThemeInlinePressed = "#B4B6B8"
+    static let darkThemeDefault = "#F2FFFFFF"
+    static let darkThemeDefaultBorder = "#F2FFFFFF"
+    static let darkThemeDefaultHover = "#CCFFFFFF"
+    static let darkThemeDefaultPressed = "#B2FFFFFF"
+    static let darkThemeDefaultPressedBorderColor = "#B2FFFFFF"
+    static let darkThemeDefaultTextColor = "#F2000000"
+    static let darkThemeDefaultSelectedTextColor = "#F2000000"
     
-    static let darkThemePrimary = "#07C1F5"
-    static let darkThemePressed = "#0A5E7D"
+    static let darkThemePositive = "#FF1A1A1A"
+    static let darkThemePositiveBorder = "#FF3CC29A"
+    static let darkThemePositiveHover = "FF185E46"
+    static let darkThemePositivePressed = "#FF134231"
+    static let darkThemePositivePressedBorderColor = "#FF134231"
+    static let darkThemePositiveTextColor = "#FF3CC29A"
+    static let darkThemePositiveSelectedTextColor = "#F2FFFFFF"
     
-    static let darkThemePositive = "#00CF64"
-    static let darkThemeDestructive = "#D4371C"
-    static let darkThemeDestructiveHover = "#A12512"
-    static let darkThemeDestructivePressed = "#6E1D13"
+    static let darkThemeDestructive = "#FF1A1A1A"
+    static let darkThemeDestructiveBorder = "#FFFC8B98"
+    static let darkThemeDestructiveHover = "FFAB0A15"
+    static let darkThemeDestructivePressed = "#FF780D13"
+    static let darkThemeDestructivePressedBorderColor = "#FF780D13"
+    static let darkThemeDestructiveTextColor = "#FFFC8B98"
+    static let darkThemeDestructiveSelectedTextColor = "#F2FFFFFF"
 }
