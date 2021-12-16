b5741293bbbd4ee42c9b1b0f1a5b5d8140fec682
ankmangl <94901239+ankmangl@users.noreply.github.com>
2021-12-16 15:51:53 +0530

    Input Text Renderer for 1.3  (#244)
    * Initial Commit for code refactor
    
    * Removed 3rd Level of refactoring for now
    
    * Added Associated Inputs
    
    * Added Stretch Property
    
    * Input Field Colors for Error Validation added
    
    * Error Message Colors Added
    
    * Code Refactor
    
    * Accessibility Added
    
    * Incorporated Review comments
    
    * Review comments
    
    Co-authored-by: Govardhan N <ggovardh@cisco.com>

---
+++ source/qml/Library/RendererQml/TextInputRender.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "AdaptiveCardQmlRenderer.h"
+#include "QmlTag.h"
+#include <memory>
+
+class TextinputElement
+{
+public:
+	TextinputElement(std::shared_ptr<AdaptiveCards::TextInput> input, std::shared_ptr<RendererQml::AdaptiveRenderContext> context);
+	TextinputElement() = delete;
+	TextinputElement(const TextinputElement&) = delete;
+	TextinputElement& operator= (const TextinputElement&) = delete;
+	std::shared_ptr<RendererQml::QmlTag> getQmlString();
+	void initialize();
+
+private:
+	std::shared_ptr<RendererQml::QmlTag> mTextinputElement;
+	std::shared_ptr<RendererQml::QmlTag> mTextinputColElement;
+	std::shared_ptr<RendererQml::QmlTag> mContainer;
+	const std::shared_ptr<AdaptiveCards::TextInput>& mTextinput;
+	const std::shared_ptr<RendererQml::AdaptiveRenderContext>& mContext;
+
+private:
+	void initMultiLine();
+	void initSingleLine();
+	std::shared_ptr<RendererQml::QmlTag> createInputTextLabel(bool isRequired = false);
+	std::shared_ptr<RendererQml::QmlTag> createErrorMessageText(std::string errorMessage, const std::shared_ptr<RendererQml::QmlTag> uiTextInput);
+	const std::string getColorFunction();
+	void addInlineActionMode();
+	std::shared_ptr<RendererQml::QmlTag> createSingleLineTextFieldElement();
+	std::shared_ptr<RendererQml::QmlTag> createMultiLineTextAreaElement();
+	std::shared_ptr<RendererQml::QmlTag> createMultiLineBackgroundElement();
+	void addValidationToInputText(std::shared_ptr<RendererQml::QmlTag>& uiTextInput);
+};
+
