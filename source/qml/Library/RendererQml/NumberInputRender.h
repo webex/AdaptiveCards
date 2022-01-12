7f170edede1432c31713360cb4b459e3215b8458
ankmangl <94901239+ankmangl@users.noreply.github.com>
2022-01-12 10:46:39 +0530

    Feature 1 3 number (#250)
    * Pushing Before Re-factoring
    
    * Refactoring
    
    * changed tab to 4 space
    
    * Fixed issue related to label
    
    * Code Cleanup
    
    * Added accesibility and Fixed some minor issues
    
    * Refactoring wrt to ID's
    
    * Fixed issue
    
    * add missing file
    
    * Reverting project setting change
    
    * Fixed border color issue's
    
    * Fixed Review comments by Govardhan
    
    * Removing Validaion in case isRequired is not set.
    
    * Fixed Issue reated to isRequired
    
    * Fixed issue related to input validation when is required is not supplied.

---
+++ source/qml/Library/RendererQml/NumberInputRender.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "AdaptiveCardQmlRenderer.h"
+#include "QmlTag.h"
+#include <memory>
+
+class NumberinputElement
+{
+public:
+    NumberinputElement(std::shared_ptr<AdaptiveCards::NumberInput> input, std::shared_ptr<RendererQml::AdaptiveRenderContext> context);
+    NumberinputElement() = delete;
+    NumberinputElement(const NumberinputElement&) = delete;
+    NumberinputElement& operator= (const NumberinputElement&) = delete;
+    std::shared_ptr<RendererQml::QmlTag> getQmlTag();
+    void initialize();
+
+private:
+    std::shared_ptr<RendererQml::QmlTag> numberInputColElement;
+    const std::shared_ptr<AdaptiveCards::NumberInput>& mInput;
+    const std::shared_ptr<RendererQml::AdaptiveRenderContext>& mContext;
+    RendererQml::InputNumberConfig numberConfig;
+    std::string mOrigionalElementId{ "" };
+    std::string mContentTagId{ "" };
+    std::string mNumberInputRectId{ "" };
+
+private: 
+    static std::shared_ptr<RendererQml::QmlTag> getDummyElementforNumberInput(bool isTop);
+    void createInputLabel();
+    void createErrorMessage();
+    std::shared_ptr<RendererQml::QmlTag> getIconTag(const std::shared_ptr<RendererQml::QmlTag> textBackgroundTag);
+    std::shared_ptr<RendererQml::QmlTag> getContentItemTag(const std::shared_ptr<RendererQml::QmlTag> textBackgroundTag);
+    const std::string getAccessibleName();
+    const std::string getColorFunction();
+    std::ostringstream getValidatorFunction();
+};
+
