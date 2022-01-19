ed895ed740c4788aea718a2673d98c9cdaffecca
snbehar <79969931+snbehar@users.noreply.github.com>
2022-01-19 18:45:43 +0530

    [MAC] - Bug fix for number input not clearing error on keyboard accessibility on stepper (#253)

--- source/macos/AdaptiveCards/AdaptiveCards/Utils/ViewUtils.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Utils/ViewUtils.swift
@@ -7,4 +7,16 @@ extension NSView {
         let viewLocation = convert(windowLocation, from: nil)
         return bounds.contains(viewLocation)
     }
+    
+    var isViewInFocus: Bool {
+        // This takes care of textField/buttons
+        if let controlElement = self as? NSControl, let cell = controlElement.cell, cell.isAccessibilityFocused() {
+            return true
+        }
+        // For special case of multiline textFields wich are subclassed fron NSTextView
+        if let textView = self as? NSTextView, textView.isAccessibilityFocused() {
+            return true
+        }
+        return false
+    }
 }
