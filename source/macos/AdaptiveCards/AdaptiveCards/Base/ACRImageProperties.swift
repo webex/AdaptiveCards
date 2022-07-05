7259263f84bddcd98a9ff13da7021d584e5149d3
Urmit Chauhan <uchauhan@cisco.com>
2022-07-05 20:07:01 +0530

    Refactor ImageView constraint
    Remove the existing imageview setup constraints logic. take help from Microsoft iOS AdaptiveCard imageview class.

--- source/macos/AdaptiveCards/AdaptiveCards/Base/ACRImageProperties.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Base/ACRImageProperties.swift
@@ -1,6 +1,11 @@
 import AdaptiveCards_bridge
 import AppKit
 
+struct ACRAspectRatio {
+    var widthToHeight: CGFloat
+    var heightToWidth: CGFloat
+}
+
 class ACRImageProperties: NSObject {
     var hasExplicitDimensions = false
     var isAspectRatioNeeded = false
@@ -31,7 +36,7 @@ class ACRImageProperties: NSObject {
         if acsImageSize == ACSImageSize.none {
             acsImageSize = ACSImageSize.auto
         }
-  
+        
         // update the content size based on image sizes and pixel dimensions
         contentSize = ImageUtils.getImageSizeAsCGSize(imageSize: acsImageSize,
                                                       width: pixelWidth,
@@ -44,11 +49,11 @@ class ACRImageProperties: NSObject {
     
     func updateContentSize(size: CGSize) {
         let ratios: CGSize = ImageUtils.getAspectRatio(from: size)
-
+        
         let heightToWidthRatio = ratios.height
         let widthToHeightRatio = ratios.width
         var newSize = contentSize
-
+        
         if hasExplicitDimensions {
             if pixelWidth > 0 {
                 newSize.width = pixelWidth
@@ -75,6 +80,24 @@ class ACRImageProperties: NSObject {
         }
     }
 }
+
+// MARK: - class function
+extension ACRImageProperties {
+    // Size to Aspect Ratio conversion
+    class func convertToAspectRatio(_ cgsize: CGSize) -> ACRAspectRatio {
+        if cgsize.width == 0 {
+            return .init(widthToHeight: 1.0, heightToWidth: 1.0)
+        }
+        // keeping all precisions are not necessary, and 744W X 84H and its multiples cause a crash.
+        // rounding off to 100th points.
+        // To make the constraints work, the rounded value has to become 1 when the two ratios are multiplied
+        let precision: CGFloat = 100
+        // MAX is necessary to prevent heightByWidth becoming zero.
+        let heightToWidth = max(round(precision * (cgsize.height / cgsize.width)) / precision, 1 / precision)
+        return .init(widthToHeight: 1 / heightToWidth, heightToWidth: heightToWidth)
+    }
+}
+
 extension NSImage {
     var absoluteSize: NSSize {
         guard !representations.isEmpty else {
