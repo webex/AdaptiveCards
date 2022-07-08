b2b40f0097a8c34f53da7bb2c70a4a39ea111bba
urmitchauhan <105660080+urmitchauhan@users.noreply.github.com>
2022-07-08 10:14:24 +0530

    Refactor ImageView constraint (#274)
    * Refactor ImageView constraint
    
    Remove the existing imageview setup constraints logic. take help from Microsoft iOS AdaptiveCard imageview class.
    
    * update content size in imageprops

--- source/macos/AdaptiveCards/AdaptiveCards/Base/ACRImageProperties.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Base/ACRImageProperties.swift
@@ -1,6 +1,11 @@
 import AdaptiveCards_bridge
 import AppKit
 
+struct ACRAspectRatio {
+    var widthToHeight: CGFloat
+    var heightToWidth: CGFloat
+}
+
 class ACRImageProperties: NSObject {
     var hasExplicitDimensions = false
     var isAspectRatioNeeded = false
@@ -31,7 +36,7 @@ class ACRImageProperties: NSObject {
         if acsImageSize == ACSImageSize.none {
             acsImageSize = ACSImageSize.auto
         }
-  
+        
         // update the content size based on image sizes and pixel dimensions
         contentSize = ImageUtils.getImageSizeAsCGSize(imageSize: acsImageSize,
                                                       width: pixelWidth,
@@ -43,26 +48,32 @@ class ACRImageProperties: NSObject {
     }
     
     func updateContentSize(size: CGSize) {
-        let ratios: CGSize = ImageUtils.getAspectRatio(from: size)
-
-        let heightToWidthRatio = ratios.height
-        let widthToHeightRatio = ratios.width
+        let ratios: ACRAspectRatio = ACRImageProperties.convertToAspectRatio(size)
+        
+        let heightToWidthRatio = ratios.heightToWidth
+        let widthToHeightRatio = ratios.widthToHeight
         var newSize = contentSize
+        
+        let newHeight: ((CGFloat) -> CGFloat) = { width in
+            return width * heightToWidthRatio
+        }
 
+        let newWidth: ((CGFloat) -> CGFloat) = { height in
+            return height * widthToHeightRatio
+        }
+        
         if hasExplicitDimensions {
             if pixelWidth > 0 {
                 newSize.width = pixelWidth
                 if pixelHeight == 0 {
-                    newSize.height = pixelWidth * heightToWidthRatio
-                    pixelHeight = newSize.height
+                    newSize.height = newHeight(self.pixelWidth)
                 }
             }
             
             if pixelHeight > 0 {
                 newSize.height = pixelHeight
                 if pixelWidth == 0 {
-                    newSize.width = pixelHeight * widthToHeightRatio
-                    pixelWidth = newSize.width
+                    newSize.width = newWidth(self.pixelHeight)
                 }
             }
             contentSize = newSize
@@ -70,11 +81,29 @@ class ACRImageProperties: NSObject {
             contentSize = size
         } else if acsImageSize == ACSImageSize.small || acsImageSize == ACSImageSize.medium || acsImageSize == ACSImageSize.large {
             newSize = contentSize
-            newSize.height = contentSize.width * heightToWidthRatio
+            newSize.height = newHeight(self.contentSize.width)
             contentSize = newSize
         }
     }
 }
+
+// MARK: - class function
+extension ACRImageProperties {
+    // Size to Aspect Ratio conversion
+    class func convertToAspectRatio(_ cgsize: CGSize) -> ACRAspectRatio {
+        if cgsize.width == 0 {
+            return .init(widthToHeight: 1.0, heightToWidth: 1.0)
+        }
+        // keeping all precisions are not necessary, and 744W X 84H and its multiples cause a crash.
+        // rounding off to 100th points.
+        // To make the constraints work, the rounded value has to become 1 when the two ratios are multiplied
+        let precision: CGFloat = 100
+        // MAX is necessary to prevent heightByWidth becoming zero.
+        let heightToWidth = max(round(precision * (cgsize.height / cgsize.width)) / precision, 1 / precision)
+        return .init(widthToHeight: 1 / heightToWidth, heightToWidth: heightToWidth)
+    }
+}
+
 extension NSImage {
     var absoluteSize: NSSize {
         guard !representations.isEmpty else {
