f11ff2e86fe551590476d4caf1842ad21a651235
Mukul Agarwal <mukuagar@cisco.com>
2022-11-28 13:12:12 +0530

    Added tests

--- source/macos/AdaptiveCards/AdaptiveCardsTests/Renderers/ContainerRendererTests.swift
+++ source/macos/AdaptiveCards/AdaptiveCardsTests/Renderers/ContainerRendererTests.swift
@@ -23,7 +23,8 @@ class ContainerRendererTests: XCTestCase {
     func testHeightProperty() {
         let autoContainer = FakeContainer.make(elemType: .container, heightType: .auto)
         let rootAutoView = renderContainerView(FakeContainer.make(elemType: .container, minHeight: 200, items: [autoContainer]))
-        XCTAssertEqual(rootAutoView.stackView.arrangedSubviews.capacity, 3)
+        // Since no padding is added and is handled by lastPadding, decrease number of elements
+        XCTAssertEqual(rootAutoView.stackView.arrangedSubviews.capacity, 2)
         XCTAssertEqual(rootAutoView.stackView.arrangedSubviews.first?.contentHuggingPriority(for: .vertical), NSLayoutConstraint.Priority.defaultLow)
         
         let stretchContainer = FakeContainer.make(elemType: .container, heightType: .stretch)
@@ -40,13 +41,14 @@ class ContainerRendererTests: XCTestCase {
         
         container = .make(verticalContentAlignment: .bottom, items: [FakeTextBlock.make()])
         containerView = renderContainerView(container)
+        // since we removed padding view all together and replaced with lastPadding but use paddingView for vertical content alignment this has changed
         // SpaceView 1
-        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 2)
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 3)
         
         container = .make(verticalContentAlignment: .center, items: [FakeTextBlock.make()])
         containerView = renderContainerView(container)
         // SpaceView 2
-        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 3)
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.capacity, 4)
     }
     
     func testSelectActionTargetIsSet() {
@@ -75,6 +77,22 @@ class ContainerRendererTests: XCTestCase {
         containerView = renderContainerView(container)
     
         XCTAssertNil(containerView.target)
+    }
+    
+    func testIntrinsicContentSizeWithHiddenInputElements() {
+        let inputField = FakeInputText.make(id: "id", isRequired: true, errorMessage: "error message", label: "label message", separator: false, heightType: .auto, isVisible: false)
+        let containerView = renderContainerView(FakeContainer.make(elemType: .container, items: [inputField]))
+        XCTAssertEqual(containerView.intrinsicContentSize, .zero)
+    }
+    
+    func testPaddingWhenContainerEmptyWithoutStyle() {
+        // No padding added
+        var containerView = renderContainerView(FakeContainer.make(style: .none, elemType: .container, visible: true))
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.count, 0)
+        
+        //Added padding
+        containerView = renderContainerView(FakeContainer.make(style: .default, elemType: .container, visible: true))
+        XCTAssertEqual(containerView.stackView.arrangedSubviews.count, 1)
     }
     
     func testRendersItems() {
