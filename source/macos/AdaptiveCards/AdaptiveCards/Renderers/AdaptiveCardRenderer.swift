a179946774ac9d76a8ed4bccfd58b9c3cbc40a85
Urmit Chauhan <uchauhan@cisco.com>
2022-12-07 15:12:17 +0530

    Add new protocol at ImageResourceHandlerView for Image setup

--- source/macos/AdaptiveCards/AdaptiveCards/Renderers/AdaptiveCardRenderer.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Renderers/AdaptiveCardRenderer.swift
@@ -101,7 +101,7 @@ class AdaptiveCardRenderer {
             rootView.configureLayoutAndVisibility(verticalContentAlignment: card.getVerticalContentAlignment(), minHeight: card.getMinHeight(), isBackgroundImageAvailable: !(card.getBackgroundImage()?.getUrl()?.isEmpty ?? true))
         }
         rootView.appearance = NSAppearance.getAppearance(isDark: config.isDarkMode)
-        rootView.dispatchResolveRequests()
+        rootView.dispatchImageResolveRequests()
         return rootView
     }
 }
