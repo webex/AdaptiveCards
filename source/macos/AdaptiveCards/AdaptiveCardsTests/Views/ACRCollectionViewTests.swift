51e3697bdab7866351155f70db23e4ac1c378e39
urmitchauhan <105660080+urmitchauhan@users.noreply.github.com>
2023-04-05 21:54:23 +0530

    Visual focus at Image, ImageSet (#356)
    * Visual focus at Image, ImageSet
    
    - padding in acrcollectionview cell item
    - focus in imageView
    - add test
    
    * minor changes

--- source/macos/AdaptiveCards/AdaptiveCardsTests/Views/ACRCollectionViewTests.swift
+++ source/macos/AdaptiveCards/AdaptiveCardsTests/Views/ACRCollectionViewTests.swift
@@ -47,7 +47,7 @@ class ACRCollectionViewTests: XCTestCase {
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(images: [image]), hostConfig: hostConfig)
         
         XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, numberOfItemsInSection: 0), 1)
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 200, height: 200))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 208, height: 208))
         
     }
     
@@ -56,33 +56,33 @@ class ACRCollectionViewTests: XCTestCase {
         let image = FakeImage.make(imageSize: .small)
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(imageSize: .large, images: [image]), hostConfig: hostConfig, collectionLayout: collectionViewLayout)
         // We respect image size of imageset even if image size is present
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 300, height: 300))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 308, height: 308))
     }
     
     func testCollectionViewImageExplicitHeight() {
         let image = FakeImage.make(pixelHeight: 120)
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(imageSize: .large, images: [image]), hostConfig: hostConfig, collectionLayout: collectionViewLayout)
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 120, height: 120))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 128, height: 128))
     }
     
     func testCollectionViewImageExplicitWidth() {
         // if explicit width less than collection view width, we can make the cell as big as explicit width
         let image = FakeImage.make(pixelWidth: 140)
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(imageSize: .large, images: [image]), hostConfig: hostConfig, collectionLayout: collectionViewLayout)
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 140, height: 140))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 148, height: 148))
     }
     
     func testCollectionViewImageExplicitWidthAndHeight() {
         let image = FakeImage.make(pixelWidth:140, pixelHeight: 120)
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(imageSize: .large, images: [image]), hostConfig: hostConfig, collectionLayout: collectionViewLayout, frameRect: NSRect(origin: .zero, size: NSSize(width: 500, height: 500)))
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 140, height: 120))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 148, height: 128))
     }
     
     func testCollectionViewItemSizeNotExcedeParentWidth() {
         let image = FakeImage.make(pixelWidth:140, pixelHeight: 120)
         collectionView = ACRCollectionView(rootView: rootView, parentView: parentView, imageSet: FakeImageSet.make(imageSize: .large, images: [image]), hostConfig: hostConfig, collectionLayout: collectionViewLayout, frameRect: NSRect(origin: .zero, size: NSSize(width: 50, height: 500)))
         // Width will be the width of collectionView since explicit width greater than collection view width
-        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 50, height: 120))
+        XCTAssertEqual(collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0)), NSSize(width: 58, height: 128))
     }
     
     func testCollectionViewHeight() {
@@ -97,11 +97,11 @@ class ACRCollectionViewTests: XCTestCase {
         let minItemSpacing = collectionView.collectionView(collectionView.collectionView, layout: collectionViewLayout, minimumInteritemSpacingForSectionAt: 0)
         
         // minimum item spacing for 3 elements will be width of 3 elements plus 2x min item spacing
-        let minRequiredWidth: CGFloat = CGFloat(integerLiteral:numberOfItems * imageDimenion) + CGFloat((numberOfItems - 1)) * minItemSpacing
+        let minRequiredWidth: CGFloat = CGFloat(integerLiteral:numberOfItems * (imageDimenion + 8)) + CGFloat((numberOfItems - 1)) * minItemSpacing
         XCTAssertLessThan(collectionView.collectionView.intrinsicContentSize.width, minRequiredWidth)
         // Now that we know less space is available than required to display in one row, collectionView should layout to multiple rows so height should increase
         let numberOfRows = Int(ceil(minRequiredWidth/CGFloat(collectionViewWidth)))
-        let calculatedHeight = CGFloat(numberOfRows * imageDimenion) + CGFloat((numberOfRows - 1)) * minItemSpacing
+        let calculatedHeight = CGFloat(numberOfRows * (imageDimenion + 8)) + CGFloat((numberOfRows - 1)) * minItemSpacing
         XCTAssertEqual(collectionView.intrinsicContentSize.height, calculatedHeight)
         collectionView.collectionView.reloadData()
         XCTAssertEqual(collectionView.collectionView.collectionViewLayout, collectionViewLayout)
@@ -129,13 +129,13 @@ class ACRCollectionViewTests: XCTestCase {
         let thirdElementFrame = collectionView.collectionView.frameForItem(at: 2)
         
         // FirstElement should be present at the top left of frame
-        let firstElementCalculatedFrame = NSRect(x: 0, y: 0, width: imageDimenion, height: imageDimenion)
+        let firstElementCalculatedFrame = NSRect(x: 0, y: 0, width: (imageDimenion + 8), height: (imageDimenion + 8))
         
         // second element will be layout after firstElement with a space of minItemSpacing
-        let secondElementCalculatedFrame = NSRect(x: imageDimenion + Int(minItemSpacing), y: 0, width: imageDimenion, height: imageDimenion)
+        let secondElementCalculatedFrame = NSRect(x: (imageDimenion + 8) + Int(minItemSpacing), y: 0, width: (imageDimenion + 8), height: (imageDimenion + 8))
         
         // third element will be layout in second row as no space in first, so there will be a shift in y by imageHeight and minItemSpacing
-        let thirdElementCalculatedFrame = NSRect(x: 0, y: imageDimenion + Int(minItemSpacing), width: imageDimenion, height: imageDimenion)
+        let thirdElementCalculatedFrame = NSRect(x: 0, y: (imageDimenion + 8) + Int(minItemSpacing), width: (imageDimenion + 8), height: (imageDimenion + 8))
         
         XCTAssertEqual(firstElementFrame, firstElementCalculatedFrame)
         XCTAssertEqual(secondElementFrame, secondElementCalculatedFrame)
