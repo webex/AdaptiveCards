9d43c59f9130cbcdfc25c679d7453b28c4a30ed4
Urmit Chauhan <uchauhan@cisco.com>
2023-04-17 09:57:06 +0530

    [macOS] Implement blue focus ring for Fact Set
    - update factset renderer
    - test

--- source/macos/AdaptiveCards/AdaptiveCards/Views/ACRFactSetView.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Views/ACRFactSetView.swift
@@ -3,6 +3,8 @@ import AppKit
 
 // MARK: Class required for Horizontal Stack View
 class ACRFactSetView: NSView {
+    private weak var lastRefKeyView: NSView?
+    
     init() {
         super.init(frame: .zero)
     }
@@ -16,5 +18,28 @@ class ACRFactSetView: NSView {
         // Should look for better solution
         guard let superview = superview else { return }
         widthAnchor.constraint(equalTo: superview.widthAnchor).isActive = true
+    }
+    
+    override func layout() {
+        super.layout()
+        recalculateKeyViewLoop()
+    }
+    
+    private func recalculateKeyViewLoop() {
+        weak var lastKeyView: NSView?
+        if let titleView = self.subviews.first, let valueView = self.subviews.last {
+            // This will begin functioning once we get the default keyview loop views. then we will alter according to our needs. otherwise default may override the loop sequence. we add nil scenario due to avoid multiple time execution.
+            guard titleView.subviews.first?.nextKeyView != nil, lastRefKeyView != titleView.subviews.first?.nextKeyView else {
+                return
+            }
+            self.lastRefKeyView = valueView.subviews.first
+            for index in 0..<titleView.subviews.count {
+                if let lastKeyView = lastKeyView {
+                    lastKeyView.nextKeyView = titleView.subviews[index]
+                }
+                titleView.subviews[index].nextKeyView = valueView.subviews[index]
+                lastKeyView = valueView.subviews[index]
+            }
+        }
     }
 }
